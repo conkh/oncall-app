a813b49e456dc6ab9d06b130268731261d1cfe48
sang cao <sangcaoson@gmail.com>
2026-03-21 10:02:09 -0500

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repoName = "";
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repoName = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
